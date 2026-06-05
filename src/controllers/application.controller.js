f2364d7382a258359e9f217347a3f414fe05480c
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-06-06 01:11:07 +0600

    Refactor application controller to include all application-related methods for better structure and maintainability

--- src/controllers/application.controller.js
+++ src/controllers/application.controller.js
@@ -1,22 +1,18 @@
 import applicationModel from "../models/application.model";
 import asyncHandler from "../utils/asyncHandler";
 
-// postApplicationController
-// getAllApplicationController
-// updateApplicationController
-// deleteApplicationController
-// myApplicationController
-// viewApplicationController
+const postApplicationController = asyncHandler(async (req, res) => {});
+const getAllApplicationController = asyncHandler(async (req, res) => {});
+const updateApplicationController = asyncHandler(async (req, res) => {});
+const deleteApplicationController = asyncHandler(async (req, res) => {});
+const myApplicationController = asyncHandler(async (req, res) => {});
+const viewApplicationController = asyncHandler(async (req, res) => {});
 
-
-const postApplicationController=asyncHandler(async (req,res) => {
-    
-
-    
-
-})
-
-
-
-export default {postApplicationController}
-
+export default {
+  postApplicationController,
+  getAllApplicationController,
+  updateApplicationController,
+  deleteApplicationController,
+  myApplicationController,
+  viewApplicationController,
+};
